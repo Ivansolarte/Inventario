eac6570d8c325197be2eee7444160776a8076c79
Ivansolarte <chaca360@gmail.com>
2019-02-10 23:16:00 -0500

    10.2

--- src/app/inventario/inventario-lista.component.ts
+++ src/app/inventario/inventario-lista.component.ts
@@ -1,13 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-// import { InventarioComponent } from './inventario.component';
+import { InventarioService } from './inventario.service';
+import { Inventario } from './inventario';
+
 
 @Component ({
     selector: 'app-inventario-lista',
     templateUrl: 'inventario-lista.component.html'
 })
 export class InventarioListaComponent implements OnInit {
-    constructor() {}
-    ngOnInit() {
 
+    lista: Inventario[];
+
+    constructor(
+        private servicio: InventarioService
+    ) {}
+    ngOnInit() {
+        this.servicio.getInventario().subscribe(
+            rs => this.lista = rs,
+            er => console.log(er),
+            () => console.log(this.lista)
+        );
     }
 }
--- src/app/inventario/inventario.service.ts
+++ src/app/inventario/inventario.service.ts
@@ -5,9 +5,9 @@ import { Observable } from 'rxjs';// c
 import { Inventario } from './inventario'; // el archivo de la clase inventario (campos de la base de BD)
 import { map } from 'rxjs/operators';
 // import { catchError } from 'rxjs/operators';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/first';
-import 'rxjs/add/operato/catch';
+// import 'rxjs/add/operator/map';
+// import 'rxjs/add/operator/first';
+// import 'rxjs/add/operato/catch';
 
 import { from } from 'rxjs';
 
@@ -24,15 +24,15 @@ export class InventarioService {
     return this.http.get(url).pipe(map(r => r.json()));
   }
   // flata el catch por  revisar
-  private handleError(error: Response | any) {
-    let errMsg: string;
-      if (error instanceof Response) {
-        let body = error.json() || '';
-        let err = body.error || JSON.stringify(body);
-        errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
-      } else {
-        errMsg = error.message ? error.message : error.toString();
-      }
-      return Observable.throw(errMsg);
-  }
+  // private handleError(error: Response | any) {
+  //   let errMsg: string;
+  //     if (error instanceof Response) {
+  //       let body = error.json() || '';
+  //       let err = body.error || JSON.stringify(body);
+  //       errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+  //     } else {
+  //       errMsg = error.message ? error.message : error.toString();
+  //     }
+  //     return Observable.throw(errMsg);
+  // }
 }
